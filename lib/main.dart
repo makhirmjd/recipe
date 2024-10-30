3c63d12142109a35d8c1f9470a8c2d60bca31362
Muhammad Abdulmalik <muhammad@Muhammads-MacBook-Pro.local>
2024-10-30 20:31:34 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/recipe_detail.dart';
+import 'recipe.dart';
+
+void main() {
+  runApp(const RecipesApp());
+}
+
+class RecipesApp extends StatelessWidget {
+  const RecipesApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+    return MaterialApp(
+      title: 'Recipe Calculator',
+      theme: theme.copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.greenAccent,
+        ),
+      ),
+      home: const MyHomePage(
+        title: 'Recipe Calculator',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+        elevation: 2.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Image(image: AssetImage(recipe.imageUrl)),
+              const SizedBox(
+                height: 14.0,
+              ),
+              Text(
+                recipe.label,
+                style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Palatino',
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
